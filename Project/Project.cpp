a0d224c51ff364206af8f68d8222fb1bd8cb7010
AnolBoi <anoltongi68@gmail.com>
2024-12-16 17:48:44 +0300

    veri çekme

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -1,20 +1,45 @@
-﻿// Project.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
-//
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <sstream>
 
-#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+unordered_map<int, vector<pair<int, int>>> movieRatings;
+unordered_map<int, vector<pair<int, int>>> userRatings;
+
+void populateRatings(const string& filename) {
+    ifstream infile(filename);
+    string line;
+
+    while (getline(infile, line)) {
+        stringstream ss(line);
+        string userIDStr, movieIDStr, ratingStr;
+
+        if (!getline(ss, userIDStr, ',') ||
+            !getline(ss, movieIDStr, ',') ||
+            !getline(ss, ratingStr, ',')) {
+            cerr << "Error: Invalid data format in line: " << line << endl;
+            continue;
+        }
+
+        int userID = 0;
+        int movieID = 0;
+        int rating = 0;
+
+        movieRatings[movieID].push_back({ userID, rating });
+        userRatings[userID].push_back({ movieID, rating });
+    }
 }
 
-// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
-// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
 
-// Kullanmaya Başlama İpuçları: 
-//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
-//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
-//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
-//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
-//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
-//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
+
+int main() {
+    string filePath = "C:/Users/Asus/Desktop/BAU/CMP2003/training_data.csv";
+    populateRatings(filePath);
+
+
+    cout << "Ratings loaded successfully." << endl;
+    return 0;
+}
